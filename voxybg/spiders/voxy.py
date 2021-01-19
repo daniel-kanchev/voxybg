d2a93b5e9243d56eba468ec89e4f9425269d2aa6
daniel-kanchev <daniel.kanchev@adata.pro>
2021-01-19 11:18:31 +0200

    comments added, minor changes

--- voxybg/spiders/voxy.py
+++ voxybg/spiders/voxy.py
@@ -2,17 +2,18 @@ import scrapy
 from scrapy.loader import ItemLoader
 from voxybg.items import Article
 from datetime import datetime
-import time
 
 
 def format_content(text, title):
-    if not text.strip():
+    # removes data which was already extracted from the article body
+
+    if not text.strip():  # removes whitespace
         return False
 
-    elif text == title:
+    elif text == title:  # removes the title
         return False
 
-    elif text.endswith("Comments"):
+    elif text.endswith("Comments"):  # removes the number of comments
         return False
 
     else:
@@ -29,24 +30,27 @@ class VoxySpider(scrapy.Spider):
         for article in articles:
             link = article.xpath(".//div[@class='read-btn']/a/@href").get()
             yield response.follow(link, self.parse_article)
+
         next_page = response.xpath("//a[@class='next page-numbers']/@href").get()
         if next_page:
-            time.sleep(2)
             yield response.follow(next_page, self.parse)
 
     def parse_article(self, response):
         item = ItemLoader(item=Article(), response=response)
 
         title = response.xpath("//h3/text()").get()
+
         date = response.xpath("//span[@class='entry-date']/descendant-or-self::*/text()").getall()
         date = date[0]
+
         author = response.xpath("//span[@class='entry-author']/descendant-or-self::*/text()").getall()
         author = author[0]
+
         content = response.xpath("//div[@class='article_content']/descendant-or-self::*/text()").getall()
         content = [text.strip() for text in content if format_content(text, title)]
-        content = " ".join(content[2:])
+        content = " ".join(content[2:])  # the slice removes the author and date from the article body
 
-        # format date
+        # reformat date
         date_time_obj = datetime.strptime(date, '%B %d, %Y')  # July 15,2020
         date = date_time_obj.strftime("%y/%m/%d")
 
@@ -57,4 +61,3 @@ class VoxySpider(scrapy.Spider):
         item.add_value('author', author)
 
         return item.load_item()
-
